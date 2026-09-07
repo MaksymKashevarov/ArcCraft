35da6f05bafd2248c2b07c86b07bb254f23b640f
Maksym Kashevarov <kmaximlk04@outlook.com>
2026-09-07 14:34:49 +0200

    fix: register agent debug command

--- src/main/java/com/maksymkashevarov/arccraft/ArcCraft.java
+++ src/main/java/com/maksymkashevarov/arccraft/ArcCraft.java
@@ -3,6 +3,8 @@ package com.maksymkashevarov.arccraft;
 import com.maksymkashevarov.arccraft.registry.ArcAttachments;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import com.maksymkashevarov.arccraft.command.ArcCommands;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(ArcCraft.MODID)
 public class ArcCraft {
@@ -11,5 +13,6 @@ public class ArcCraft {
 
     public ArcCraft(IEventBus modEventBus) {
         ArcAttachments.register(modEventBus);
+        NeoForge.EVENT_BUS.addListener(ArcCommands::register);
     }
 }
