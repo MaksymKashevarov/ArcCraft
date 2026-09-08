d42e55c163972c408102677dff0081c42520cf17
Maksym Kashevarov <kmaximlk04@outlook.com>
2026-09-08 14:10:18 +0200

    feat: add contract core block skeleton

---
+++ src/main/java/com/maksymkashevarov/arccraft/world/block/ContractCoreBlock.java
@@ -0,0 +1,10 @@
+package com.maksymkashevarov.arccraft.world.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class ContractCoreBlock extends Block {
+
+    public ContractCoreBlock(Properties properties) {
+        super(properties);
+    }
+}
